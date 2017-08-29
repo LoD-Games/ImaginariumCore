1f1fdf91f26be1ad870e3a52441dddacf206ae75
DESKTOP-ASQ0J34\MI <skyland7494@mail.ru>
2017-08-29 19:45:21 +0300

    4 stage data

--- ImaginariumCore/ImaginariumCore/Contracts/Output/Stages/FourthStageData.cs
+++ ImaginariumCore/ImaginariumCore/Contracts/Output/Stages/FourthStageData.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Interfaces;
 
 namespace ImaginariumCore.Contracts.Output.Stages
 {
@@ -7,7 +9,19 @@ namespace ImaginariumCore.Contracts.Output.Stages
         public FourthStageData(ILobby lobby)
         {
             Stage = lobby.Stage;
+            MainCard = lobby.Players.SingleOrDefault(player => player.Token.Equals(lobby.MainPlayer)).Card;
+            VoteResult = new List<VoteResult>();
+            lobby.TryGoToNextStage();
         }
         public int Stage { get; set; }
+        public IList<VoteResult> VoteResult { get; set; }
+        public int MainCard { get; set; }
+        public IList<string> DonePlayers { get; set; }
+    }
+
+    public class VoteResult
+    {
+        public int Id { get; set; }
+        public  int Cound { get; set; }
     }
 }
